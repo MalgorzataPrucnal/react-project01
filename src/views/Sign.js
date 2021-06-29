6257947d896487cec1aba738a6ed93c2bfdf6e45
Malgorzata Prucnal <=>
2021-06-29 19:22:47 +0200

    Margins added

--- src/views/Sign.js
+++ src/views/Sign.js
@@ -7,9 +7,22 @@ const Form = s.form`
   width: 30%;
   align-items: center;
   margin: 50px auto;
-  padding: 50px;
+  padding: 100px 150px 150px;
   background-color: palevioletred;
 `;
+
+const Input = s.input`
+// display: flex;
+// flex-direction: column;
+// align-items: center;
+margin: 10px 0px 15px 0px;
+`
+const Label = s.label`
+// display: flex;
+// flex-direction: column;
+// align-items: center;
+// padding: 15px 0px 10px 0px;
+`
 
 
 export const Sign = ({email,password,error}) => {
@@ -19,15 +32,15 @@ return (
       
     <h3>Log in</h3>
     <label htmlFor="email">E-mail:</label>
-    <input
+    <Input
       id="email"
       type="email"
       value={email}
       name="email"
 
       required/>
-    <label htmlFor="password">Password:</label>
-    <input
+    <Label htmlFor="password">Password:</Label>
+    <Input
       id="password"
       type="password"
       value={password}
